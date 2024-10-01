94fe17b871c5b6eef57ec74a3dbb85f2a4fd7702
Ahmed Osama Awad Al-Saeed <135539823+AhmedOsama0js@users.noreply.github.com>
2024-10-01 13:33:27 +0300

    Arab Contractors(2)

--- js/navbarHeader.js
+++ js/navbarHeader.js
@@ -3,7 +3,7 @@ const Header = document.getElementById("headerNavbar");
 Header.innerHTML = `
   <nav class="navbar navbar-expand-lg bg-body-tertiary bg-light">
   <div class="container-fluid ">
-    <a class="navbar-brand" href="index.html"><img class="logo" src="./images/ابناء المقاولون .jpg" /></a>
+    <a class="navbar-brand" href="index.html"><img class="logo" src="./images/ابناء المقاولون .jpg" alt="" /></a>
     <button class="navbar-toggler border border-secondary border-2 " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon d-flex align-items-center justify-content-center ">
       <i class="bi bi-list"></i>
@@ -20,18 +20,9 @@ Header.innerHTML = `
         <li class="nav-item">
           <a class="nav-link" href="contact.html">CONTACT US</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            SERVICES
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#AboutOurApartment">About Our Apartment</a></li>
-            <li><a class="dropdown-item" href="#services">Services</a></li>
-            <li><a class="dropdown-item" href="#DealForHouse">Deal For House</a></li>
-            <li><a class="dropdown-item" href="#client">clients</a></li>
-            <li><a class="dropdown-item" href="#GetInTouch">Get In Touch</a></li>
+        <li class="nav-item">
+          <a class="nav-link" href="Services.html">SERVES</a>
         </li>
-      </ul>
     </div>
   </div>
   </nav>
